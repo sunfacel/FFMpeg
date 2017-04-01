5e218be26c95686deaba53576066466923f4d3a4
sunface <sunfacel@163.com>
2017-04-01 20:54:53 +0800

    ouputVideoAudio

---
+++ outputVedioAudio/outputVideoAudio.cpp
@@ -0,0 +1,89 @@
+﻿extern "C" {
+
+#include <libavcodec\avcodec.h>
+#include <libavformat\avformat.h>
+#include <libswscale\swscale.h>
+#include <libswresample\swresample.h>
+
+}
+
+#include <sdl2/SDL.h>
+#include <sdl2/SDL_thread.h>
+
+#include <iostream>
+
+#include "PacketQueue.h"
+#include "Audio.h"
+#include "Media.h"
+#include "VideoDisplay.h"
+using namespace std;
+
+bool quit = false;
+
+
+#pragma comment( lib, "avcodec.lib" )
+#pragma comment( lib, "avformat.lib" )
+#pragma comment( lib, "avutil.lib" )
+#pragma comment( lib, "swscale.lib")
+#pragma comment( lib, "avdevice.lib")
+#pragma comment( lib, "avfilter.lib")
+
+#pragma comment( lib, "SDL2.lib")
+#pragma comment( lib, "SDL2main.lib")
+
+#pragma comment( lib, "swresample.lib")
+#pragma comment( lib, "swscale.lib")
+
+
+int main(int argv, char* argc[])
+{
+	av_register_all();
+
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
+
+// 	char* filename = "E:\\Wildlife.wmv";
+ 	char* filename = "ad.rmvb";
+	MediaState media(filename);
+
+	if (media.openInput())
+		SDL_CreateThread(decode_thread, "", &media); // 创建解码线程，读取packet到队列中缓存
+
+	media.audio->audio_play(); // create audio thread
+
+	media.video->video_play(&media); // create video thread
+
+	AVStream *audio_stream = media.pFormatCtx->streams[media.audio->stream_index];
+	AVStream *video_stream = media.pFormatCtx->streams[media.video->stream_index];
+
+	double audio_duration = audio_stream->duration * av_q2d(audio_stream->time_base);
+	double video_duration = video_stream->duration * av_q2d(video_stream->time_base);
+
+	cout << "audio时长:" << audio_duration << endl;
+	cout << "video时长:" << video_duration << endl;
+
+	SDL_Event event;
+	while (true) // SDL event loop
+	{
+		SDL_WaitEvent(&event);
+		switch (event.type)
+		{
+		case FF_QUIT_EVENT:
+		case SDL_QUIT:
+			quit = 1;
+			SDL_Quit();
+
+			return 0;
+			break;
+
+		case FF_REFRESH_EVENT:
+			video_refresh_timer(&media);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	getchar();
+	return 0;
+}
